a1a97116077e33a6bc11b16c015453371b1d944d
Hakan Kucukdereli <hkucukdereli@gmail.com>
2024-03-22 16:39:23 -0400

    File reorganized

--- Figure_2/calculate_preferences.py
+++ Figure_2/calculate_preferences.py
@@ -67,6 +67,46 @@ def calcPrefShuffle(df, nshuffles, condition=None):
     
     return np.sort(np.array(pref_list))
 
+def calcPrefNulldist(df, nshuffles, condition=None):
+    """
+    Calculate the null distribution of preferences on each day for a given locomotion state. 
+    Conditions can be `dwell`, `explore`, `transition` or None for any locomotion state.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Dataframe containing the data.
+    nshuffles : int
+        Number of shuffles.
+    condition : str, optional
+        Locomotion state to use. The default is None. 
+        Condition can be `dwell`, `explore`, `transition` or None for any locomotion state.
+
+    Returns
+    -------
+    pref_list : list
+        List of preference values.
+
+    """
+    if condition:
+        stim = df.query(f'session in [1,2] & stim!=0 & {condition}==True').groupby('cueid').dt.sum()
+        neutral = df.query(f'session in [1,2] & neutral!=0 & {condition}==True').groupby('cueid').dt.sum()
+    else:
+        stim = df.query(f'session in [1,2] & stim!=0').groupby('cueid').dt.sum()
+        neutral = df.query(f'session in [1,2] & neutral!=0').groupby('cueid').dt.sum()
+
+    all_trials = pd.concat([stim, neutral], ignore_index=True)
+
+    pref_list = []
+    for n in range(nshuffles):
+        stim_t = all_trials.sample(n=len(stim), replace=False).sum()
+        neutral_t = all_trials.sample(n=len(neutral), replace=False).sum()
+
+        pref_val = (stim_t - neutral_t) / (stim_t + neutral_t)
+        pref_list.append(pref_val)
+
+    return np.sort(np.array(pref_list))
+
 def calcPrefSession(df, session, condition=None):
     """
     Calculate preference value for a given locomotion state for a given session. Conditions can be `dwell`, `explore`, `transition` or None for any locomotion state.
